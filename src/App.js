665d6c105a05dfc8c076c23495899f8f44d2e772
krivalex <aleksei_krivoborodov@mail.ru>
2022-10-04 21:04:59 +0600

    react-deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
+const tg = window.Telegram.WebApp
 
 function App() {
+
+  useEffect(() => {
+    tg.ready()
+  }, [])
+
+  const onClose = () => {
+    tg.close()
+  }
+
   return (
     <div className="App">
       <h1>It's working</h1>
+      <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
