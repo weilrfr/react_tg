9fc4e12c13a776767ba1708f5822ff30c71f7b40
weilrfr <boyarkinid3@gmail.com>
2023-10-08 16:13:36 +0600

    modal

--- src/components/SpeedDialButton/SpeedDialButton.jsx
+++ src/components/SpeedDialButton/SpeedDialButton.jsx
@@ -1,12 +1,24 @@
 import './SpeedDialButton.css';
 import { SpeedDial } from 'primereact/speeddial';
+import React, { useState } from "react";
+import { Dialog } from 'primereact/dialog';
+import { InputText } from "primereact/inputtext";
+
 
 export const SpeedDialButton = ({products}) => {
-    
+    const [visibleUpdate, setVisibleUpdate] = useState(false);
+    const [visibleAdd , setVisibleAdd] = useState(false);
+    const [visibleTrash, setVisibleTrash] = useState(false);
+
+    const [updatevalue, updateSetValue] = useState('');
+    const [addValue, addSetValue] = useState('');
+    const [trashValue, trashSetValue] = useState('');
+
+
     const actionItems = [
-        { icon: 'pi pi-pencil', command: () => { } },
-        { icon: 'pi pi-cart-plus', command: () => { } },
-        { icon: 'pi pi-trash', command: () => { } },
+        { icon: 'pi pi-pencil', command: () => { setVisibleUpdate(true) } },
+        { icon: 'pi pi-cart-plus', command: () => { setVisibleAdd(true) } },
+        { icon: 'pi pi-trash', command: () => { setVisibleTrash(true) } },
     ];
 
     const addHelicopter = (data) => {
@@ -25,10 +37,34 @@ export const SpeedDialButton = ({products}) => {
             return item
         })
     }
-
+    
     return (
         <div>
             <SpeedDial model={actionItems}  radius={120} type="quarter-circle" direction="down-left" style={{ right: 0, top: 0 }} buttonClassName="p-button-help" />
+            <div className="card flex justify-content-center">
+                <Dialog header="Введите ID для изменения" visible={visibleUpdate} style={{ width: '50vw' }} onHide={() => setVisibleUpdate(false)}>
+                    <span className="p-float-label">
+                        <InputText id="updateHel" value={updatevalue} onChange={(e) => updateSetValue(e.target.value)} />
+                        <label htmlFor="updateHel">id</label>
+                    </span>
+                </Dialog>
+            </div>
+            <div className="card flex justify-content-center">
+                <Dialog header="Новое объявление" visible={visibleAdd} style={{ width: '50vw' }} onHide={() => setVisibleAdd(false)}>
+                    <span className="p-float-label">
+                        <InputText id="addHel" value={addValue} onChange={(e) => addSetValue(e.target.value)} />
+                        <label htmlFor="addHel">Название</label>
+                    </span>
+                </Dialog>
+            </div>
+            <div className="card flex justify-content-center">
+                <Dialog header="Введите ID для удаления объявления" visible={visibleTrash} style={{ width: '50vw' }} onHide={() => setVisibleTrash(false)}>
+                    <span className="p-float-label">
+                        <InputText id="delHel" value={trashValue} onChange={(e) => trashSetValue(e.target.value)} />
+                        <label htmlFor="delHel">id</label>
+                    </span>
+                </Dialog>
+            </div>
         </div>
     )
 }
